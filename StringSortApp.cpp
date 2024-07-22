ba8592fbd2c709fe5cc3348329d1a6e021a1f54f
Ilya Brilenkov <ilyabrilenkov@gmail.com>
2024-07-22 15:30:24 +0500

    Add files via upload

---
+++ StringSortApp.cpp
@@ -0,0 +1,32 @@
+﻿#include "String.h"
+#include "Sort.h"
+#include <vector>
+
+#include <thread>
+#include <chrono>
+
+int main()
+{    
+    std::vector<String> listOfStrings{};
+    bool repeat{ true };
+
+    std::cout << "Enter as many strings as you want. Enter 'q' to see a sorted list of the entered strings\n";
+
+    while (repeat)
+    {       
+        String newString{};
+        std::cin >> newString;
+
+        if (newString[0] == 'q' && newString.length() == 1)
+        {
+            std::cout << "\nSorted strings: \n";
+            Sort::mergeSort(listOfStrings, 0, listOfStrings.size()-1);
+            Sort::printInvercedSortedVector(listOfStrings);
+            repeat = false;
+        }
+        else
+            listOfStrings.push_back(newString);
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+}
+
